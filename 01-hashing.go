73612f4dc9e2757994d537ef604c9e1ee96ae158
Daniel Sont <dan.sont@gmail.com>
2018-06-10 12:22:27 +0700

    hashing starting

---
+++ 01-hashing.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	println("https://www.geeksforgeeks.org/hashing-set-1-introduction/")
+	println("https://www.geeksforgeeks.org/hashing-set-2-separate-chaining/")
+	println("https://www.geeksforgeeks.org/birthday-paradox/")
+	println("taylor series... https://en.wikipedia.org/wiki/Taylor_series -- wow, I forget how to math things.. calculus + probability + infinite sums.. all at once yikes")
+	println("consider basically refreshing your brain on calc + statistics. which you should have payed attention to in school")
+	println("I will come back to this later.. back to hash-tables..")
+	println("separate chaining is straightforward. colliding hashed keys go into linked lists.")
+	println("seems pretty bad in reality, due to cache locality problems")
+}
