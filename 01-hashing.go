8b2d191b6a8ec6810e085d6bc52279e70bfde01b
Daniel Sont <dan.sont@gmail.com>
2018-06-10 13:25:04 +0700

    more looking into hash map implementations

--- 01-hashing.go
+++ 01-hashing.go
@@ -9,4 +9,8 @@ func main() {
 	println("I will come back to this later.. back to hash-tables..")
 	println("separate chaining is straightforward. colliding hashed keys go into linked lists.")
 	println("seems pretty bad in reality, due to cache locality problems")
+	println("https://www.geeksforgeeks.org/hashing-set-3-open-addressing/ video helped - but lacking explanation of how to increase size of hash table and how operations still would work.. and how the hash functions (\% table size) change")
+	println("should enlighten me: https://web.archive.org/web/19990903133921/http://www.concentric.net/~Ttwang/tech/primehash.htm")
+	println("super interesting: https://golang.org/src/runtime/hashmap.go")
+	println("I wonder if I can define key generation function makes the go hashmap implementation suck horribly. It seems to double its size, and not use prime table sizes")
 }
