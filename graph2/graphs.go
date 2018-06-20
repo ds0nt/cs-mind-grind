150f85be6bde86b3b15ec3d8ae58f93153ac121f
Daniel Sont <dan.sont@gmail.com>
2018-06-20 16:03:55 +0700

    adding cyclic test for directed graph. still need better method to find all disconnected subgraphs of a graph

--- graph2/graphs.go
+++ graph2/graphs.go
@@ -1,6 +1,8 @@
 package graph2
 
-import "github.com/ds0nt/cs-mind-grind/disjoint_set"
+import (
+	"github.com/ds0nt/cs-mind-grind/disjoint_set"
+)
 
 // Node is a node in a graph
 type Node struct {
@@ -45,11 +47,6 @@ func (ug *UndirectedGraph) AddEdge(a, b *Node) {
 	ug.Edges = append(ug.Edges, &Edge{a, b})
 }
 
-// AddEdge is a convinience method for ug.AddEdge
-func (n *Node) AddEdge(ug *UndirectedGraph, b *Node) {
-	ug.AddEdge(n, b)
-}
-
 func (ug *UndirectedGraph) IsCyclic() bool {
 
 	djs := disjoint_set.NewDisjointSet()
@@ -71,6 +68,96 @@ func (ug *UndirectedGraph) IsCyclic() bool {
 }
 
 type DirectedGraph struct {
+	Nodes []*Node
+}
+
+func NewDirectedGraph() *DirectedGraph {
+	return &DirectedGraph{
+		Nodes: []*Node{},
+	}
+}
+
+// AddNode adds a node to the graph
+func (dg *DirectedGraph) AddNode(nodes ...*Node) {
+	dg.Nodes = append(dg.Nodes, nodes...)
+}
+
+// AddEdge adds an edge between two nodes in the graph
+func (dg *DirectedGraph) AddEdge(from, to *Node) {
+	from.Adjascent = append(from.Adjascent, to)
+}
+
+// IsCyclic detects if the directed graph has a cycle in it
+// simply do a DFS and if we hit any node twice, it's cyclic
+func (dg *DirectedGraph) IsCyclic() bool {
+	if len(dg.Nodes) == 0 {
+		return false
+	}
+
+	// DETERMINE START NODES IS CURRENTLY FLAWED
+	// a cyclic subgraph with all vertexes indegree > 0 will not be searched
+	// I better find all subgraphs by doing this: https://stackoverflow.com/a/1348995/2532523
+	// caught this in my extensive unit testing ^.^
+	// count all indegrees
+	indegrees := map[*Node]int{}
+	for _, n := range dg.Nodes {
+		indegrees[n] = 0
+	}
+
+	for _, n := range dg.Nodes {
+		for _, adj := range n.Adjascent {
+			indegrees[adj]++
+		}
+	}
+
+	// find nodes with no indegrees
+	startNodes := []*Node{}
+	for n, in := range indegrees {
+		if in == 0 {
+			startNodes = append(startNodes, n)
+		}
+	}
+
+	// if we have startNodes then it's already cyclic
+	if len(startNodes) == 0 {
+		return true
+	}
+	// END FLAWEDNESS
+
+	for _, n := range startNodes {
+		visited := map[*Node]bool{}
+		if hasBackEdges(n, []*Node{}, visited) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func hasBackEdges(n *Node, ancestors []*Node, visited map[*Node]bool) bool {
+	visited[n] = true
+	// ancestor of self also counts as cycle.. :p
+	ancestors = append(ancestors, n)
+
+	for _, adj := range n.Adjascent {
+		// have we visited this node?
+		if _, ok := visited[adj]; ok {
+			// is this node a parent? if yes, we have a backedge
+			for _, old := range ancestors {
+				if old == adj {
+					return true
+				}
+			}
+			// if it's not a parent ignore
+			continue
+		}
+
+		// if we have not visited it yet, recurse into it
+		if hasBackEdges(adj, ancestors, visited) {
+			return true
+		}
+	}
+	return false
 }
 
 type WeightedGraph struct {
--- graph2/graphs_test.go
+++ graph2/graphs_test.go
@@ -76,3 +76,72 @@ func TestIsCyclic(t *testing.T) {
 	assert.True(t, testGraph.IsCyclic())
 
 }
+
+func TestDirectedGraph(t *testing.T) {
+	graph := NewDirectedGraph()
+	n1, n2 := NewNode(1), NewNode(2)
+	graph.AddNode(n1, n2)
+
+	assert.Len(t, graph.Nodes, 2)
+	assert.Equal(t, graph.Nodes[0], n1)
+	assert.Equal(t, graph.Nodes[1], n2)
+
+	graph.AddEdge(n1, n2)
+	assert.Len(t, n1.Adjascent, 1)
+	assert.Len(t, n2.Adjascent, 0)
+	assert.Equal(t, n1.Adjascent[0], n2)
+
+	graph.AddEdge(n1, n2)
+	assert.Len(t, n1.Adjascent, 2)
+
+	graph.AddEdge(n2, n1)
+	assert.Len(t, n1.Adjascent, 2)
+	assert.Len(t, n2.Adjascent, 1)
+	assert.Equal(t, n2.Adjascent[0], n1)
+
+}
+
+func TestDirectedIsCyclic(t *testing.T) {
+
+	// single edge test, then no startpoint cyclic test
+	graph := NewDirectedGraph()
+	n1, n2 := NewNode(1), NewNode(2)
+	graph.AddNode(n1, n2)
+	graph.AddEdge(n1, n2)
+	assert.False(t, graph.IsCyclic())
+	graph.AddEdge(n2, n1)
+	assert.True(t, graph.IsCyclic())
+
+	// two edges test, then 1 start point cyclic test
+	graph = NewDirectedGraph()
+	n1, n2, n3 := NewNode(1), NewNode(2), NewNode(3)
+	graph.AddNode(n1, n2, n3)
+	graph.AddEdge(n1, n2)
+	graph.AddEdge(n2, n3)
+	assert.False(t, graph.IsCyclic())
+	graph.AddEdge(n3, n2)
+	assert.True(t, graph.IsCyclic())
+
+	// multi-path acyclic test, multi-tree test, single vertex test, alternate tree cycle test
+	graph = NewDirectedGraph()
+	n1, n2, n3 = NewNode(1), NewNode(2), NewNode(3)
+	graph.AddNode(n1, n2, n3)
+	graph.AddEdge(n1, n2)
+	graph.AddEdge(n1, n3)
+	graph.AddEdge(n2, n3)
+	graph.AddEdge(n2, n3) // double edge
+	assert.False(t, graph.IsCyclic())
+	// add three disconnected single vertexes
+	n4, n5, n6 := NewNode(4), NewNode(5), NewNode(6)
+	graph.AddNode(n4, n5, n6)
+	assert.False(t, graph.IsCyclic())
+	// connect other vertexes acyclically
+	graph.AddEdge(n6, n5)
+	graph.AddEdge(n6, n4)
+	assert.False(t, graph.IsCyclic())
+
+	// create a cycle
+	graph.AddEdge(n4, n6)
+	assert.True(t, graph.IsCyclic())
+
+}
